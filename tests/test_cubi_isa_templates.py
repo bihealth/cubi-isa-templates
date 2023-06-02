57e1e4c7f1b6aa7605bc377c648d3c1b832858ba
Thomas Sell <thomas.sell@charite.de>
2023-06-02 12:38:11 +0200

    parametrize test instead of loop

--- tests/test_cubi_isa_templates.py
+++ tests/test_cubi_isa_templates.py
@@ -7,28 +7,27 @@ import pytest
 
 import cubi_isa_templates
 
-
 @pytest.mark.filterwarnings("error::altamisa.exceptions.CriticalIsaValidationWarning")
-def test_isatabs(tmp_path):
-    for tpl in cubi_isa_templates.TEMPLATES.values():
-        extra_context = {"__output_dir": tpl.name}
-        cookiecutter(
-            template=tpl.path, extra_context=extra_context, output_dir=tmp_path, no_input=True
-        )
+@pytest.mark.parametrize("tpl", cubi_isa_templates.TEMPLATES.values())
+def test_isatabs(tpl, tmp_path):
+    extra_context = {"__output_dir": tpl.name}
+    cookiecutter(
+        template=tpl.path, extra_context=extra_context, output_dir=tmp_path, no_input=True
+    )
 
-        output = tmp_path / tpl.name
-        assert output.exists()
+    output = tmp_path / tpl.name
+    assert output.exists()
 
-        class dummyArgs(typing.NamedTuple):
-            input_investigation_file: typing.TextIO
-            show_duplicate_warnings: bool = False
+    class dummyArgs(typing.NamedTuple):
+        input_investigation_file: typing.TextIO
+        show_duplicate_warnings: bool = False
 
-        invs = []
-        for f in output.iterdir():
-            if fnmatch(f.name, "i_*.txt"):
-                invs.append(f)
-        assert len(invs) == 1
+    invs = []
+    for f in output.iterdir():
+        if fnmatch(f.name, "i_*.txt"):
+            invs.append(f)
+    assert len(invs) == 1
 
-        with (output / invs[0].name).open() as f:
-            args = dummyArgs(input_investigation_file=f)
-            isatab_validate.run(args)
+    with (output / invs[0].name).open() as f:
+        args = dummyArgs(input_investigation_file=f)
+        isatab_validate.run(args)
