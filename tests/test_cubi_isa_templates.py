a68d520756e7033478557146cc10b86158a126b1
Thomas Sell <thomas.sell@charite.de>
2023-06-05 11:57:50 +0200

    simple iteration over all choice variables

--- tests/test_cubi_isa_templates.py
+++ tests/test_cubi_isa_templates.py
@@ -1,33 +1,41 @@
+from fnmatch import fnmatch
+import shutil
 import typing
 
 from altamisa.apps import isatab_validate
 from cookiecutter.main import cookiecutter
-from fnmatch import fnmatch
 import pytest
 
 import cubi_isa_templates
 
+
 @pytest.mark.filterwarnings("error::altamisa.exceptions.CriticalIsaValidationWarning")
 @pytest.mark.parametrize("tpl", cubi_isa_templates.TEMPLATES.values())
 def test_isatabs(tpl, tmp_path):
-    extra_context = {"__output_dir": tpl.name}
-    cookiecutter(
-        template=tpl.path, extra_context=extra_context, output_dir=tmp_path, no_input=True
-    )
+    for key, value in tpl.configuration.items():
+        if isinstance(value, list) and not key.startswith("_"):
+            for choice in value:
+                extra_context = {"__output_dir": tpl.name, key: choice}
+                cookiecutter(
+                    template=tpl.path,
+                    extra_context=extra_context,
+                    output_dir=tmp_path,
+                    no_input=True,
+                )
 
-    output = tmp_path / tpl.name
-    assert output.exists()
+                output = tmp_path / tpl.name
+                assert output.exists()
 
-    class dummyArgs(typing.NamedTuple):
-        input_investigation_file: typing.TextIO
-        show_duplicate_warnings: bool = False
+                class dummyArgs(typing.NamedTuple):
+                    input_investigation_file: typing.TextIO
+                    show_duplicate_warnings: bool = False
 
-    invs = []
-    for f in output.iterdir():
-        if fnmatch(f.name, "i_*.txt"):
-            invs.append(f)
-    assert len(invs) == 1
+                invs = [f for f in output.iterdir() if fnmatch(f.name, "i_*.txt")]
+                assert len(invs) == 1
 
-    with (output / invs[0].name).open() as f:
-        args = dummyArgs(input_investigation_file=f)
-        isatab_validate.run(args)
+                with (output / invs[0].name).open() as f:
+                    args = dummyArgs(input_investigation_file=f)
+                    isatab_validate.run(args)
+
+                # cleanup
+                shutil.rmtree(output)
