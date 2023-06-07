8c2c7adcbdbbd9fc33c70fa8edca345d0a64dc6f
sellth <thomas.sell@charite.de>
2023-06-07 14:29:44 +0200

    add template validation tests (#2, #12)

---
+++ tests/test_cubi_isa_templates.py
@@ -0,0 +1,41 @@
+from fnmatch import fnmatch
+import shutil
+import typing
+
+from altamisa.apps import isatab_validate
+from cookiecutter.main import cookiecutter
+import pytest
+
+import cubi_isa_templates
+
+
+@pytest.mark.filterwarnings("error::altamisa.exceptions.CriticalIsaValidationWarning")
+@pytest.mark.parametrize("tpl", cubi_isa_templates.TEMPLATES.values())
+def test_isatabs(tpl, tmp_path):
+    for key, value in tpl.configuration.items():
+        if isinstance(value, list) and not key.startswith("_"):
+            for choice in value:
+                extra_context = {"__output_dir": tpl.name, key: choice}
+                cookiecutter(
+                    template=tpl.path,
+                    extra_context=extra_context,
+                    output_dir=tmp_path,
+                    no_input=True,
+                )
+
+                output = tmp_path / tpl.name
+                assert output.exists()
+
+                class dummyArgs(typing.NamedTuple):
+                    input_investigation_file: typing.TextIO
+                    show_duplicate_warnings: bool = False
+
+                invs = [f for f in output.iterdir() if fnmatch(f.name, "i_*.txt")]
+                assert len(invs) == 1
+
+                with (output / invs[0].name).open() as f:
+                    args = dummyArgs(input_investigation_file=f)
+                    isatab_validate.run(args)
+
+                # cleanup
+                shutil.rmtree(output)
