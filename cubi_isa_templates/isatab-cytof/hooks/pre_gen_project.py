cdb4177348d1d9560cf7f3f5ae069b868f5e34f8
Thomas Sell <thomas.sell@charite.de>
2023-06-07 14:08:53 +0200

    remove validation via hooks

--- cubi_isa_templates/isatab-cytof/hooks/pre_gen_project.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sample_factors = "{{cookiecutter.sample_factors}}".split(",")
-sample_factor_types = "{{cookiecutter.sample_factor_types}}".split(",")
-if not len(sample_factors) == len(sample_factor_types):
-    print("ERROR: Sample factors need to have the same length as Sample factor types.")
-    sys.exit(1)
